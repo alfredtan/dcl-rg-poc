0bb95c39ac1d06ef7931663350582abf12040448
Alfred Tan <alfred.tan@hogarthww.com>
2022-07-06 17:48:24 +0800

    adjust Y position of the model

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,7 @@
 const scene = new Entity()
 scene.addComponent(new GLTFShape("models/hww_collision_test_02.glb"))
 scene.addComponent(new Transform({
-  position: new Vector3(8, 1, 8)
+  position: new Vector3(8, 0, 8)
 }))
 
 engine.addEntity(scene)
